af928a0532c1fd1e5385f1ee21954a37ebca4d79
Erçin Dedeoğlu <e.dedeoglu@gmail.com>
2020-02-29 19:22:11 +0300

    Ok

---
+++ ECommerce.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce.Web/wwwroot/js/page/Home_Index.js
@@ -0,0 +1,15 @@
+﻿var Home_Index = {
+    Init: function () {
+        $.ajax({
+            type: "GET",
+            url: "/module/userbar",
+            data: [],
+            success: Home_Index.ModuleUserBar_Callback,
+            dataType: "html",
+            contentType: "html"
+        });
+    },
+    ModuleUserBar_Callback: function (result) {
+        $("#Module-UserBar").html(result);
+    }
+};
